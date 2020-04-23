b458fc2b5a7000b8ccc02ca94d82ca8944c9a2e4
LevPewPew <levthedev@protonmail.com>
2020-04-24 07:51:24 +1000

    redid footer a bit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="Footer">
-    <p>By Lev | <a href="https://www.levtoth.dev">Portfolio</a> | <a href="https://twitter.com/DevLev3">Twitter</a> | <a href="https://github.com/LevPewPew">GitHub</a></p>
+    <p><a href="https://www.levtoth.dev">By Lev</a> | <a href="https://twitter.com/DevLev3">Twitter</a> | <a href="https://github.com/LevPewPew">GitHub</a></p>
   </footer>
 );
 
