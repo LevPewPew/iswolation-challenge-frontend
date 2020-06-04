a3d8033fda15a6497681619d0f4fa80648335bd1
LevPewPew <levthedev@protonmail.com>
2020-06-04 19:38:46 +1000

    fixed bug with copy link

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
-  REACT_SERVER: 'http://localhost:3000',
+  REACT_SERVER: 'https://iswolationchallenge.netlify.app',
   WEB_SERVER: process.env.REACT_APP_BACKEND_URL
 };
