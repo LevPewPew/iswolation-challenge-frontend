01bb8e019748ce83a6913308b12dea7ff19fad2b
Levente Toth <levthedev@protonmail.com>
2023-08-10 18:18:49 +1000

    Update validation.js comment

--- src/config/validation.js
+++ src/config/validation.js
@@ -7,7 +7,7 @@ export const validation = {
   MAX_PLAYERS_LENGTH: 100, // 100, UX design decision
   MIN_EXERCISE_NAME_CHARS: 1, // no blank names
   MAX_EXERCISE_NAME_CHARS: 18,  // 18 chosen to make frontend display logic easier
-  MIN_EXERCISES_LENGTH: 1, // less than 1 player is pointless
+  MIN_EXERCISES_LENGTH: 1, // less than 1 exercise is pointless
   MAX_EXERCISES_LENGTH: 10,  // 10, UX design decision
   MIN_REPS_COUNT: 1, // less than 1 rep is pointless
   MAX_REPS_COUNT: 999 // 999 chosen to make frontend display logic easier, and also to prevent people from killing themselves reaching crazy reps
